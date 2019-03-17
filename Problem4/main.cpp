8b2e4f60b2bb84683bc41ed9e66f02f30d4e2407
Andreas Vasilakis <andreas.alex.vasilakis@gmail.com>
2019-03-17 13:30:15 +0200

    Add Solution to Problem 4

---
+++ Problem4/main.cpp
@@ -0,0 +1,101 @@
+/**
+ * @file       solution4.cpp
+ * @author     Andreas Vasilakis
+ * @date       17/03/2019
+ * @version    1.0
+ * 
+ * @brief      Largest palindrome product
+ * 
+ * @scope
+ * 
+ * Find the largest palindrome made from the product of two 3-digit numbers.
+ * 
+ */
+
+#include <iostream>
+#include <stdio.h>
+#include <cmath>
+#include <assert.h>
+
+#ifdef EXECUTION_TIME_COMPUTATION
+#include <chrono> 
+using namespace std::chrono; 
+#endif 
+
+long maxPrimeFactor(long n)
+{
+    long _max = 1;
+    for(long i = 2; i < n/i; i++) {
+        while(n%i == 0) {
+            _max = std::max(_max, i);
+            n = n/i;
+        }
+    }
+
+    if(n > 0) {
+        _max = std::max(_max, n);
+    }
+
+    return _max;
+}
+
+int main (int argc, char *argv[])
+{
+    if (argc < 1) {
+        std::cerr << "Usage: " << " n \n";
+        return 1;
+    }
+    
+    const std::string _name = std::string(argv[0]);
+    
+#ifdef EXECUTION_TIME_COMPUTATION
+    auto start = high_resolution_clock::now(); 
+#endif
+    
+// Solution (naive)
+    int _max = 0;
+    bool found = false;
+    for(int i = 9; i >= 0; --i)
+    {
+        for(int j = 9; j >= 0; --j)
+        {
+            for(int k = 9; k >= 0; --k) {
+
+                std::string _num = std::to_string(i) + std::to_string(j) + std::to_string(k) + std::to_string(k) + std::to_string(j) + std::to_string(i);
+                _max = std::stoi(_num);
+
+                if(_max < 10000 || _max >= 998001) {
+                    continue;
+                }
+
+                for(int x = 999; x >=100 ; --x){
+                    for(int y = 999; y >=100 ; --y) {
+                        if(x*y == _max) {
+                            found = true;
+                        }
+                    }
+                    if(found)
+                        break;
+                }
+
+                if(found)
+                    break;
+            }
+            if(found)
+                break;
+        }
+        if(found)
+            break;
+    }
+
+    std::cout << "Solution: " << _max << std::endl;
+
+#ifdef EXECUTION_TIME_COMPUTATION
+    auto stop     = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start); 
+	
+	std::cout << "Time: " << duration.count() << " ms" << std::endl;
+#endif
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -4,8 +4,9 @@ A collection of program code to solve as many as possible of the current [Projec
 
 ### Current Progress
 
-| Problem  | Title                    | Status |
-| -------- | -------------            | ------ |
-| #1       | _Multiples of 3 and 5_   | Solved |
-| #2       | _Even Fibonacci numbers_ | Solved |
-| #3       | _Largest prime factor_   | Solved |
+| Problem  | Title                          | Status  | 
+| -------- | -------------                  | ------  |
+| #1       | _Multiples of 3 and 5_         | Solved  |
+| #2       | _Even Fibonacci numbers_       | Solved  |
+| #3       | _Largest prime factor_         | Solved  |
+| #4       | _Largest palindrome product_   | Solved  |
